07c9d80d75d84b9b54842703e9ee48842a580a01
Veronica Giraldo <veronica9893@gmail.com>
2019-04-16 11:20:56 -0600

    new project

---
+++ projects/todo-postSchool/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="todoPostSchool">
+          <h1>GOALS</h1>
+          <h2>1 Week</h2>
+          <h2>2 Weeks</h2>
+          <h2>3 Weeks</h2>
+          <h2>1 Month</h2>
+          <h2>3 Months</h2>
+          <h2>6 Months</h2>
+          <h2>1 year</h2>
+          {/* <Todo />
+          <TodoList /> */}
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
