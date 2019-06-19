9b14c04b8d24cd4c9bc96b42480f7906c3821afe
Veronica Giraldo <veronica9893@gmail.com>
2019-06-19 13:42:30 -0600

    new project

---
+++ projects/weather-api/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Weather from './Weather';
+
+function App() {
+  return (
+    <div className="App">
+    Hello 
+    <Weather />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ projects/weather-api/src/Weather.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Weather extends React.Component{
+  render(){
+    return(
+      <div>
+        Weather api
+        
+      </div>
+    )
+  }
+}
+export default Weather;
